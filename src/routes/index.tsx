16f97b42318c157181b76da6156df3b413a5943c
Mauricio Inhaia <mauricio.inhaia@hotmail.com>
2023-06-20 22:47:45 -0300

    Private Routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,13 +2,30 @@ import { Route, Routes } from "react-router-dom";
 import { Login } from "../page/Login";
 import { Register } from "../page/Register";
 import { Dashboard } from "../page/Dashboard";
+import { Schedules } from "../page/Schedules";
+import { PrivateRoute } from "./PrivateRoutes";
 
 export const RouteApp = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />}></Route>
       <Route path="/register" element={<Register />}></Route>
-      <Route path="/dashboard" element={<Dashboard />}></Route>
+      <Route
+        path="/dashboard"
+        element={
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        }
+      ></Route>
+      <Route
+        path="/schedules"
+        element={
+          <PrivateRoute>
+            <Schedules />
+          </PrivateRoute>
+        }
+      ></Route>
     </Routes>
   );
 };
